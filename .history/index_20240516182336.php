7658d11e29fc5f3386d417231a4cf808bdb217a4
samuelsemako <emmasamsemaco2001@gmail.com>
2024-05-21 14:50:22 +0100

    Uploaded 21/05/24

---
+++ .history/index_20240516182336.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en"> 
+<head>
+	<?php include "meta.php"?>
+	<title>ZOSE</title>
+</head>
+<body>
+    <header>
+		<div class="top-div">
+			<div class="inner-div">
+				<div><p>If you can envision it, than we can build it!</p></div>
+				<div class="icon-div">
+					<a href="#" title="facebook">
+					<i class="bi-facebook"></i></a>
+					<a href="#" title="twitter">
+					<i class="bi-twitter"></i></a>
+					<a href="#" title="instagram">
+					<i class="bi-instagram"></i></a>
+					<a href="#" title="youtube">
+					<i class="bi-youtube"></i></a>
+				</div>
+			</div>
+		</div>
+
+		<div class="bottom-div">
+			<div class="inner-div">
+				<div>
+					<img src="all-images/images" alt="">
+				</div>
+				<div>nav</div>
+				<div>icon</div>
+			</div>
+		</div>
+	</header>
+    
+</body>
+</html>
---
+++ .history/meta_20240516182428.php
@@ -0,0 +1,11 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" type="text/css" href="styles/main.css">
+<link rel="shortcut icon" type="image-png" href="all-images/images/logo.png">
+<link href="styles/icons-1.10.2/font/bootstrap-icons.css" type="text/css" rel="stylesheet" >
+
+<script type="text/javascripts" src="js/scripts.js"></script>
+
+
+ 
